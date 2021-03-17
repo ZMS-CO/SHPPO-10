6b68b1914419fbccbe79fee7d66847a04cc9f1dc
”ITIfall” <den.zorin2001@gmail.com>
2021-03-17 16:52:13 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World, I'm top gamer in zawarudo!");
-        System.out.println("Hello!");
+        System.out.println("Hello! Darova");
         new MyFrame();
     }
 }
