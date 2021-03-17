0f952128943bde1085440ed26a8a900785488fd0
”ITIfall” <den.zorin2001@gmail.com>
2021-03-17 19:00:14 +0300

    Очистка от херни

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World, I'm top gamer in zawarudo!");
-        System.out.println("Hello! Do anything");
-        System.out.println("Hello");
-        System.out.println(1);
+        System.out.println("Check this out!");
         new MyFrame();
 
     }
