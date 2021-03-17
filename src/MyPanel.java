2d2c392db6a797344ce52b538ce62ebb7ca0154a
Viacheslav Sipakov <sipakovv.rambler.ru>
2021-03-17 18:18:09 +0300

    Tweaked resolution

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -4,8 +4,8 @@ import javax.swing.JPanel;
 public class MyPanel extends JPanel{
 
     MyPanel(){
-        this.setPreferredSize(new Dimension(1000, 1000));
         this.setBackground(Color.black);
+        this.setPreferredSize(new Dimension(1200, 675));
     }
 
     public void paint(Graphics g) {
@@ -13,6 +13,6 @@ public class MyPanel extends JPanel{
 
         g2D.setStroke(new BasicStroke(5));
         g2D.setPaint(Color.red);
-        g2D.drawOval(300,300,400,400);
+        g2D.drawOval(200,200,400,400);
     }
 }
