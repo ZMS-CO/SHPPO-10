4a1052dbecfc4b9e9b111bab400ad7f51ec88910
Viacheslav Sipakov <sipakovv.rambler.ru>
2021-03-17 17:24:13 +0300

    Do svidania

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World, I'm top gamer in zawarudo!");
-        System.out.println("Hello! Darova");
+        System.out.println("Hello! Do svidania");
         new MyFrame();
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 public class MyFrame extends JFrame{
     MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500, 500);
+        this.setSize(1000, 1000);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
