01334d7f3e4059295e68d83adc456deb651dcabd
Viacheslav Sipakov <sipakovv.rambler.ru>
2021-03-18 16:02:31 +0300

    Added circle movement

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main
 
     public static void main(String[] args)
     {
-	// write your code here
+        // write your code here
         System.out.println("Check this out! o.o");
         new MyFrame();
 
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,21 +1,39 @@
 import java.awt.*;
-import javax.swing.JPanel;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class MyPanel extends JPanel
+public class MyPanel extends JPanel implements ActionListener
 {
+
+    Timer timer;
+    int x = 300;
+    int y = 300;
+    int r = 100;
 
     MyPanel()
     {
-
+        this.setBackground(Color.black);
         this.setPreferredSize(new Dimension(1200, 675));
+        timer = new Timer(50, this);
+        timer.start();
     }
 
     public void paint(Graphics g)
     {
+        super.paint(g);
         Graphics2D g2D = (Graphics2D) g;
         g2D.setStroke(new BasicStroke(10));
         g2D.setPaint(Color.red);
-        g2D.drawOval(530,250,200,200);
+        g2D.drawOval(x - r, y - r, r * 2, r * 2);
+
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        x = x + 1;
+        repaint();
+    }
+
+
 
 }
