cd00b5ff41fc04a2d6aed9bec8d1e36963ab165e
Viacheslav Sipakov <sipakovv.rambler.ru>
2021-03-18 17:38:46 +0300

    Tweaked frame init

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
+        //Scanner scanner = new Scanner();
         // write your code here
 
         System.out.println("Check this out! o.o");
-        new MyFrame();
+
+        MyFrame frame = new MyFrame();
+
+        //while (frame.isEnabled())
+        
+
     }
 }
