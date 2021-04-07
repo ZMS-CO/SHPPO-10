a0d88f5a80e764ec651e03cb86dd22de773df246
”ITIfall” <den.zorin2001@gmail.com>
2021-04-07 18:25:04 +0300

    Need Tick

--- src/Circle.java
+++ src/Circle.java
@@ -1,44 +1,6 @@
-public class Circle implements Figure{
-    double x = 0;
-    double y = 0;
-    double x1=0;
-    double y1=0;
-    double xVelocity=0;
-    double yVelocity=0;
-    double ResizeSpeed = 0;
-    double dH=0;
-    double Speed = 0;
-    double alpha = 0;
-    double H = 0;
-    public void initCoord(double _x, double _y)
-    {
-        x=_x;
-        y=_y;
-    }
-    public void targetCoord(double alpha, double H)
-    {
-        double x1 = x + (Math.cos(alpha)*H);
-        double y1 = y + (Math.sin(alpha)*H);
-    }
-    public void speed(double _Speed)
-    {
-        Speed = _Speed;
-    }
-    public void Color()
-    {
-
-    }
-    public void resizeSpeed(double _ResizeSpeed)
-    {
-        ResizeSpeed=_ResizeSpeed;
-    }
-    public void Move()
-    {
-        x = x + Speed*(Math.cos(alpha)*dH);
-        y = y + Speed*(Math.sin(alpha)*dH);
-        if(dH<H)
-        {
-            dH++;
-        }
-    }
+public class Circle extends Figure
+{
+    double r = 0;
+    double rVelocity = 0;
+    double rTarget = 0;
 }
--- src/Figure.java
+++ src/Figure.java
@@ -1,10 +1,44 @@
-public interface Figure {
-    public void InitCoord();
-    public void MoveCoord();
-    public void Speed();
-    public void ResizeSpeed();
-    public void Color();
-    public void Move();
-    public void Resize();
-    public void Tick();
+public class Figure {
+    double x = 0;
+    double y = 0;
+    double ResizeSpeed = 0;
+    double dH=0;
+    double Speed = 0;
+    double alpha = 0;
+    double H = 0;
+    boolean isMoving = false;
+    public void initCoord(double _x, double _y)
+    {
+        x=_x;
+        y=_y;
+    }
+    public void reset()
+    {
+        double ResizeSpeed = 0;
+        double dH=0;
+        double Speed = 0;
+        double alpha = 0;
+        double H = 0;
+    }
+    public void speed(double _Speed)
+    {
+        Speed = _Speed;
+    }
+    public void Color()
+    {
+
+    }
+    public void resizeSpeed(double _ResizeSpeed)
+    {
+        ResizeSpeed=_ResizeSpeed;
+    }
+    public void Move()
+    {
+        x = x + (Math.cos(alpha)*dH);
+        y = y + (Math.sin(alpha)*dH);
+        if(dH<H)
+        {
+            dH=dH+Speed;
+        }
+    }
 }
