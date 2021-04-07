b6bf8d1cd168b589ace35e3a9573aa1c9b5c7db6
”ITIfall” <den.zorin2001@gmail.com>
2021-04-07 17:56:46 +0300

    Test things

--- src/Circle.java
+++ src/Circle.java
@@ -1,6 +1,44 @@
 public class Circle implements Figure{
-    public void InitCoord(ArrayList<ArrayList<int[]>> GREAT_ARRAY)
+    double x = 0;
+    double y = 0;
+    double x1=0;
+    double y1=0;
+    double xVelocity=0;
+    double yVelocity=0;
+    double ResizeSpeed = 0;
+    double dH=0;
+    double Speed = 0;
+    double alpha = 0;
+    double H = 0;
+    public void initCoord(double _x, double _y)
     {
-        int[] coord = GREAT_ARRAY
+        x=_x;
+        y=_y;
+    }
+    public void targetCoord(double alpha, double H)
+    {
+        double x1 = x + (Math.cos(alpha)*H);
+        double y1 = y + (Math.sin(alpha)*H);
+    }
+    public void speed(double _Speed)
+    {
+        Speed = _Speed;
+    }
+    public void Color()
+    {
+
+    }
+    public void resizeSpeed(double _ResizeSpeed)
+    {
+        ResizeSpeed=_ResizeSpeed;
+    }
+    public void Move()
+    {
+        x = x + Speed*(Math.cos(alpha)*dH);
+        y = y + Speed*(Math.sin(alpha)*dH);
+        if(dH<H)
+        {
+            dH++;
+        }
     }
 }
--- src/Figure.java
+++ src/Figure.java
@@ -2,6 +2,7 @@ public interface Figure {
     public void InitCoord();
     public void MoveCoord();
     public void Speed();
+    public void ResizeSpeed();
     public void Color();
     public void Move();
     public void Resize();
